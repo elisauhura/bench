7d6bb5866ba26a418332b1c27f72c3b8f1ba0911
Vitor Silva <sirmochi45@gmail.com>
2018-10-15 21:55:50 -0300

    Add id variable and parse commands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
 
 extern crate serde_json;
 
@@ -8,11 +7,51 @@ mod stat;
 fn main() {
     let file_key = "BENCH_STDFILE";
     let path_key = "BENCH_STDPATH";
+    let id_key = "BENCH_STDID";
     let deafult_stdfile = String::from("bench.json");
     let deafult_stdpath = String::from("");
+    let deafult_stdid = String::from("ANON");
+    let help_msg = String::from(r#"
+bench, a benchmark automation tool
+
+syntax: bench <cmd>
+
+available commands are:
+
+build:
+    build you project
+check:
+    check is the project is ready to be runned
+help:
+    display this message
+run:
+    run the project and measure the results
+
+offset path is done via the BENCH_STDPATH, eg:
+BENCH_STDPATH=../foo/ bench build
+
+author id is set via the BENCH_STDID, otherwise, ANON will be used.
+
+    "#);
     let file: String;
     let path: String;
     let seek: String;
+    let id: String;
+    let mut mark = 0;
+    let mut command: String = String::from("");
+
+    for argument in std::env::args() {
+        mark += 1;
+        if mark == 2 {
+            command = argument.clone();
+            break;
+        }
+    }
+
+    if command == String::from("") {
+        command = String::from("help");
+    }
+
     match std::env::var(file_key) {
         Ok(val) => file = val,
         Err(_e) => file = deafult_stdfile
@@ -21,16 +60,31 @@ fn main() {
         Ok(val) => path = val,
         Err(_e) => path = deafult_stdpath
     }
+    match std::env::var(id_key) {
+        Ok(val) => id = val,
+        Err(_e) => id = deafult_stdid
+    }
+
+    match command.as_ref() {
+        "help" => {
+            print!("{}", help_msg);
+            return;
+        }
+        _ => {
+            panic!("Err: Unknown command to bench");
+        }
+    }
+
     seek = String::from(path.as_str()) + file.as_str();
     println!("Looking for {}", seek);
 
-    let mut f = File::open(seek).expect("Err: file not found");
+    let mut f = std::fs::File::open(seek).expect("Err: file not found");
     let mut raw_text= String::new();
 
     f.read_to_string(&mut raw_text).expect("Err: could not read file");
 
     // TODO: Implement a real parse to check if the file is a valid bench file
-    let mut v: serde_json::Value = serde_json::from_str(raw_text.as_str()).expect("Err: could not parse file");
+    let v: serde_json::Value = serde_json::from_str(raw_text.as_str()).expect("Err: could not parse file");
 
     check_folders(&path);
     println!("Folders are OK!");
